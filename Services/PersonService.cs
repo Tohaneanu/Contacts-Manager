2956c1c78f92f67e164f1e722172a2190d0e6e7b
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-12 22:05:15 +0200

    feat(CM1): add implementation for AddPerson

--- Services/PersonService.cs
+++ Services/PersonService.cs
@@ -1,4 +1,5 @@
 ﻿
+using Entities;
 using ServiceContracts;
 using ServiceContracts.DTO;
 
@@ -6,10 +7,49 @@ namespace Services
 {
     public class PersonService : IPersonService
     {
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+        //contructor
+        public PersonService()
+        {
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            PersonResponse response = person.ToPersonResponse();
+            response.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+            return response;
+        }
+
         public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
         {
-            throw new NotImplementedException();
+            //Validation: personAddRequet parameter can't be null
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            //Validation: PersonName can't be null
+            if (personAddRequest.PersonName == null)
+            {
+                throw new ArgumentException(nameof(personAddRequest.PersonName));
+            }
+
+            ///validations for rest of the data.... to do soon...
+
+            //Convert object from PersonAddRequest to Person type
+            Person person = personAddRequest.ToPerson();
+            //generate PersonID
+            person.PersonID = Guid.NewGuid();
+            //add person object into _persons
+            _persons.Add(person);
+             
+            return ConvertPersonToPersonResponse(person);
         }
+
+        
 
         public List<PersonResponse> GetAllPersons()
         {
