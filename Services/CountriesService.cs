c0edcc954684816d88f8e7defd2863eff074d49d
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-12 17:30:37 +0200

    feat(CM1): implement CountriesServices.AddCountry

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,13 +1,50 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+        
+        //contructor
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //Validation: countryAddRequet parameter can't be null
+            if(countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validation: CountryName can't be null
+            if (countryAddRequest.CountryName == null) 
+            { 
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation: CountryName can't be duplicate
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            if (_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exist");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+            //generate CountryID
+            country.CountryID = Guid.NewGuid();
+            //add country object into _countries
+            _countries.Add(country);
+            return country.ToCountryResponse();
         }
     }
 }
