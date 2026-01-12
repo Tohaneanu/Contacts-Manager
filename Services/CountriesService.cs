b63a7d2a3b37fc35f54b5a47c0b7ea0e3def3b91
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-12 20:19:56 +0200

    feat(CM1): implement GetCountryByCountryID

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -54,7 +54,11 @@ namespace Services
 
         public CountryResponse? GetCountryByCountryID(Guid? countryID)
         {
-            throw new NotImplementedException();
+            if (countryID == null) return null;
+            Country? country_response_from_list = _countries.FirstOrDefault(country => country.CountryID == countryID);
+            if(country_response_from_list == null) {return null;}
+
+            return country_response_from_list.ToCountryResponse();
         }
     }
 }
