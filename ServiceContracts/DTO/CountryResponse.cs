ba800eced0a4c5e2ff8a851ff99dc8e9e5401c48
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-12 20:01:17 +0200

    feat(CM1): add GetAllCountries

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -10,10 +10,24 @@ namespace ServiceContracts.DTO
     {
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) return false;
+            if(obj.GetType() != typeof(CountryResponse)) return false;
+            CountryResponse other = (CountryResponse)obj;
+            return CountryID == other.CountryID && CountryName == other.CountryName;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 
     public static class CountryExtensions
     {
+        //Converts from Country object to CountryResponse object
         public static CountryResponse ToCountryResponse(this Country country)
         {
             return new CountryResponse() { CountryID = country.CountryID, CountryName = country.CountryName };
