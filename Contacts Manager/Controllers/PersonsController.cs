5ab728dc8dc93bf8300dc6732204763278bcc35c
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-13 21:34:18 +0200

    feat(CM2): create structure

---
+++ Contacts Manager/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contacts_Manager.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
