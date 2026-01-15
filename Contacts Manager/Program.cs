209b3feefd9fbd31e6d666aca9215094aeeb3eae
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-15 17:48:22 +0200

    feat(CM7): add database

--- Contacts Manager/Program.cs
+++ Contacts Manager/Program.cs
@@ -1,3 +1,5 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
 using ServiceContracts;
 using Services;
 
@@ -7,6 +9,9 @@ builder.Services.AddControllersWithViews();
 //add services into IoC container
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
 builder.Services.AddSingleton<IPersonService, PersonService>();
+
+builder.Services.AddDbContext<PersonsDbContext>(options =>
+{ options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 
 var app = builder.Build();
 
