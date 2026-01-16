23073b5d9142781951159555f73aecbecdb09c20
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-16 16:33:12 +0200

    feat(CM7): download persons pdf

--- Contacts Manager/Controllers/PersonsController.cs
+++ Contacts Manager/Controllers/PersonsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Rotativa.AspNetCore;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
@@ -142,6 +143,27 @@ namespace Contacts_Manager.Controllers
             }
             await _personService.DeletePerson(personResponse.PersonID);
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> PersonsPDF()
+        {
+            //get all persons
+           List<PersonResponse> persons = await _personService.GetAllPersons();
+
+            //return view as pdf
+            return new ViewAsPdf("PersonsPDF", persons, ViewData)
+            {
+                PageMargins = new Rotativa.AspNetCore.Options.Margins()
+                {
+                    Top = 20,
+                    Right = 20,
+                    Bottom = 20,
+                    Left = 20,
+                },
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Landscape                
+            };
         }
     }
 }
