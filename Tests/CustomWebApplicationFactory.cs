5f86493a275f47f1b671afd61d8957f1aafdd140
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-19 17:34:22 +0200

    feat(CM9): add integration testing for persons index method

---
+++ Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,38 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+
+namespace Tests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.UseEnvironment("Test");
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(temp => temp.ServiceType == typeof(IDbContextOptionsConfiguration<ApplicationDbContext>));
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+                //var dbConnectionDescriptor = services.SingleOrDefault(
+                //    d => d.ServiceType ==
+                //        typeof(DbConnection));
+                //if (dbConnectionDescriptor != null)
+                //{
+                //    services.Remove(dbConnectionDescriptor);
+                //}
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+                });
+            });
+        }
+    }
+}
