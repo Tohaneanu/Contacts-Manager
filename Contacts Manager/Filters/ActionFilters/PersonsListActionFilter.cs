450156d8414621c9c4fae48ae0d4a1bf223d4aae
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-21 17:02:37 +0200

    feat(CM11) UI enhancements

--- Contacts Manager/Filters/ActionFilters/PersonsListActionFilter.cs
+++ Contacts Manager/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,6 +1,7 @@
 ﻿using Contacts_Manager.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace Contacts_Manager.Filters.ActionFilters
 {
@@ -34,9 +35,17 @@ namespace Contacts_Manager.Filters.ActionFilters
                 {
                     personsController.ViewData["CurrentSortedBy"] = Convert.ToString(parameters["sortBy"]);
                 }
+                else
+                {
+                    personsController.ViewData["CurrentSortedBy"] = nameof(PersonResponse.PersonName);
+                }
                 if (parameters.ContainsKey("sortOrder"))
                 {
                     personsController.ViewData["CurrentSortOrder"] = Convert.ToString(parameters["sortOrder"]);
+                }
+                else
+                {
+                    personsController.ViewData["CurrentSortOrder"] = nameof(SortOrderOptions.ASC);
                 }
             }
             //Search
