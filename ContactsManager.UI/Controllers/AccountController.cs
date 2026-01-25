99469283dd31a59752fb38cf5ea48d4746961fc9
Tohaneanu Andrei <tohaneanu_andrei7@yahoo.com>
2026-01-25 21:48:58 +0200

    feat(CM15): add identity models and register page

---
+++ ContactsManager.UI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Contacts_Manager.Controllers;
+using ContactsManager.Core.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsManager.UI.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO registerDTO)
+        {
+            return RedirectToAction(nameof(PersonsController.Index), "Persons");
+        }
+    }
+}
